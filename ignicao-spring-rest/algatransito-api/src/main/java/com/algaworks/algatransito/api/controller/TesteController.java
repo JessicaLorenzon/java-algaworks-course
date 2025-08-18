885d50a2167d16e3e47d64deb398715f7c7e8228
JessicaLorenzon <jelorenzon@gmail.com>
2025-08-18 18:33:12 -0300

    aula2.6

---
+++ ignicao-spring-rest/algatransito-api/src/main/java/com/algaworks/algatransito/api/controller/TesteController.java
@@ -0,0 +1,13 @@
+package com.algaworks.algatransito.api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TesteController {
+
+    @GetMapping("/teste")
+    public String testar() {
+        return "Testando 1,2,3,4";
+    }
+}
