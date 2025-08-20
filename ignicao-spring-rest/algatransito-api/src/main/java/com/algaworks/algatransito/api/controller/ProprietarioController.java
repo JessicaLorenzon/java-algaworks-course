446ae59ef4902c3517a347cde8959cc048396990
JessicaLorenzon <jelorenzon@gmail.com>
2025-08-20 16:46:22 -0300

    aula3.6-Consulta com JPQL

--- ignicao-spring-rest/algatransito-api/src/main/java/com/algaworks/algatransito/api/controller/ProprietarioController.java
+++ ignicao-spring-rest/algatransito-api/src/main/java/com/algaworks/algatransito/api/controller/ProprietarioController.java
@@ -1,29 +1,25 @@
 package com.algaworks.algatransito.api.controller;
 
 import com.algaworks.algatransito.domain.model.Proprietario;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
 import java.util.List;
 
 @RestController
 public class ProprietarioController {
 
+    @PersistenceContext
+    private EntityManager manager;
+
     @GetMapping("/proprietarios")
     public List<Proprietario> listar() {
-        var proprietario1 = new Proprietario();
-        proprietario1.setId(1L);
-        proprietario1.setNome("Joao Silva");
-        proprietario1.setEmail("joao@joao.com");
-        proprietario1.setTelefone("11 11111-1111");
+//        TypedQuery<Proprietario> query = manager.createQuery("from Proprietario", Proprietario.class);
+//        return query.getResultList();
 
-        var proprietario2 = new Proprietario();
-        proprietario2.setId(2L);
-        proprietario2.setNome("Maria Costa");
-        proprietario2.setEmail("maria@maria.com");
-        proprietario2.setTelefone("22 22222-2222");
+        return manager.createQuery("from Proprietario", Proprietario.class).getResultList();
 
-        return Arrays.asList(proprietario1, proprietario2);
     }
 }
