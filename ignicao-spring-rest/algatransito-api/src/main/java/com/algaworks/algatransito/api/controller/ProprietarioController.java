da21cfb24841bc5bdca44dae2b846b87e458b0ee
JessicaLorenzon <jelorenzon@gmail.com>
2025-08-21 12:06:20 -0300

    aula3.9-Implementando Query Methods

--- ignicao-spring-rest/algatransito-api/src/main/java/com/algaworks/algatransito/api/controller/ProprietarioController.java
+++ ignicao-spring-rest/algatransito-api/src/main/java/com/algaworks/algatransito/api/controller/ProprietarioController.java
@@ -3,7 +3,6 @@ package com.algaworks.algatransito.api.controller;
 import com.algaworks.algatransito.domain.model.Proprietario;
 import com.algaworks.algatransito.domain.repository.ProprietarioRepository;
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,6 +20,8 @@ public class ProprietarioController {
 
     @GetMapping("/proprietarios")
     public List<Proprietario> listar() {
+
+//        return proprietarioRepository.findByNomeContaining("a");
         return proprietarioRepository.findAll();
     }
 }
