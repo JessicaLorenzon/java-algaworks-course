a5113be7029ca76c2779c5ee1467e308d398d412
JessicaLorenzon <jelorenzon@gmail.com>
2025-08-26 16:52:42 -0300

    aula4.11-Protegendo propriedades somente-leitura

--- ignicao-spring-rest/algatransito-api/src/main/java/com/algaworks/algatransito/domain/model/Veiculo.java
+++ ignicao-spring-rest/algatransito-api/src/main/java/com/algaworks/algatransito/domain/model/Veiculo.java
@@ -1,5 +1,6 @@
 package com.algaworks.algatransito.domain.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -19,16 +20,19 @@ public class Veiculo {
     private Long id;
 
     @ManyToOne
-//    @JoinColumn(name = "proprietario_id")
     private Proprietario proprietario;
 
     private String marca;
     private String modelo;
     private String placa;
 
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     @Enumerated(EnumType.STRING)
     private StatusVeiculo status;
 
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private LocalDateTime dataCadastro;
+
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private LocalDateTime dataApreensao;
 }
