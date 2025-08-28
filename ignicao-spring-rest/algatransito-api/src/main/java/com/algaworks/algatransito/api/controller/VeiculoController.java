63c5954b0444116a58b312a287610f9be9b32cad
JessicaLorenzon <jelorenzon@gmail.com>
2025-08-28 16:08:21 -0300

    aula5.9-Criando o Representation Model do recurso de veículo

--- ignicao-spring-rest/algatransito-api/src/main/java/com/algaworks/algatransito/api/controller/VeiculoController.java
+++ ignicao-spring-rest/algatransito-api/src/main/java/com/algaworks/algatransito/api/controller/VeiculoController.java
@@ -1,5 +1,6 @@
 package com.algaworks.algatransito.api.controller;
 
+import com.algaworks.algatransito.api.model.VeiculoRepresentationModel;
 import com.algaworks.algatransito.domain.model.Veiculo;
 import com.algaworks.algatransito.domain.repository.VeiculoRepository;
 import com.algaworks.algatransito.domain.service.RegistroVeiculoService;
@@ -25,8 +26,20 @@ public class VeiculoController {
     }
 
     @GetMapping("/{veiculoId}")
-    private ResponseEntity<Veiculo> buscar(@PathVariable Long veiculoId) {
+    public ResponseEntity<VeiculoRepresentationModel> buscar(@PathVariable Long veiculoId) {
         return veiculoRepository.findById(veiculoId)
+                .map(veiculo -> {
+                    var veiculoRepresentationModel = new VeiculoRepresentationModel();
+                    veiculoRepresentationModel.setId(veiculo.getId());
+                    veiculoRepresentationModel.setNomeProprietario(veiculo.getProprietario().getNome());
+                    veiculoRepresentationModel.setMarca(veiculo.getMarca());
+                    veiculoRepresentationModel.setModelo(veiculo.getModelo());
+                    veiculoRepresentationModel.setNumeroPlaca(veiculo.getPlaca());
+                    veiculoRepresentationModel.setStatus(veiculo.getStatus());
+                    veiculoRepresentationModel.setDataCadastro(veiculo.getDataCadastro());
+                    veiculoRepresentationModel.setDataApreensao(veiculo.getDataApreensao());
+                    return veiculoRepresentationModel;
+                })
                 .map(ResponseEntity::ok)
                 .orElse(ResponseEntity.notFound().build());
     }
