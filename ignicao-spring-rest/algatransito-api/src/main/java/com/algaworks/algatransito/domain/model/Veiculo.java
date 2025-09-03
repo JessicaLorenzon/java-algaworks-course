78245246aed5f586f870668c9ffba7a65e4a0d3e
JessicaLorenzon <jelorenzon@gmail.com>
2025-09-03 15:48:12 -0300

    aula6.7-Implementando regras de negócio de apreensão de veículo

--- ignicao-spring-rest/algatransito-api/src/main/java/com/algaworks/algatransito/domain/model/Veiculo.java
+++ ignicao-spring-rest/algatransito-api/src/main/java/com/algaworks/algatransito/domain/model/Veiculo.java
@@ -1,5 +1,6 @@
 package com.algaworks.algatransito.domain.model;
 
+import com.algaworks.algatransito.domain.exception.NegocioException;
 import jakarta.persistence.*;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -41,5 +42,31 @@ public class Veiculo {
         autuacao.setVeiculo(this);
         getAutuacoes().add(autuacao);
         return autuacao;
+    }
+
+    public void apreender() {
+        if (estaApreendido()) {
+            throw new NegocioException("Veículo já se encontra apreendido!");
+        }
+
+        setStatus(StatusVeiculo.APREENDIDO);
+        setDataApreensao(OffsetDateTime.now());
+    }
+
+    public void removerApreensao() {
+        if (naoEstaApreendido()) {
+            throw new NegocioException("Veículo não esta apreendido!");
+        }
+
+        setStatus(StatusVeiculo.REGULAR);
+        setDataApreensao(null);
+    }
+
+    private boolean estaApreendido() {
+        return StatusVeiculo.APREENDIDO.equals(getStatus());
+    }
+
+    private boolean naoEstaApreendido() {
+        return !estaApreendido();
     }
 }
